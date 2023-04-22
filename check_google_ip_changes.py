92db73313d890613f17563069a1555bb2b635231
Tim Berlin <49077932+tberlin-om@users.noreply.github.com>
2023-04-22 13:03:01 +0200

    Add files via upload

---
+++ check_google_ip_changes.py
@@ -0,0 +1,48 @@
+import json
+import smtplib
+import os
+import requests
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+SMTP_USERNAME = os.environ["SMTP_USERNAME"]
+SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
+SENDER_EMAIL = os.environ["SENDER_EMAIL"]
+RECIPIENT_EMAIL = os.environ["RECIPIENT_EMAIL"]
+
+# Replace with your SMTP Server Credentials
+SMTP_SERVER = "SMTP_SERVER"
+SMTP_PORT = 25
+USE_TLS = False
+
+def send_email(subject, body):
+    msg = MIMEMultipart()
+    msg["From"] = SENDER_EMAIL
+    msg["To"] = RECIPIENT_EMAIL
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "plain"))
+
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as smtp:
+        if USE_TLS:
+            smtp.starttls()
+        smtp.login(SMTP_USERNAME, SMTP_PASSWORD)
+        smtp.sendmail(SMTP_USERNAME, RECIPIENT_EMAIL, msg.as_string())
+
+def main():
+    with open("previous_data.json", "r") as file:
+        previous_data = json.load(file)
+
+    response = requests.get(
+        "https://www.gstatic.com/ipranges/goog.json"
+    )
+    current_data = response.json()
+
+    if previous_data != current_data:
+        differences = json.dumps({"old": previous_data, "new": current_data}, indent=2)
+        send_email("Google IP Changes Detected", differences)
+
+        with open("previous_data.json", "w") as file:
+            json.dump(current_data, file, indent=2)
+
+if __name__ == "__main__":
+    main()
